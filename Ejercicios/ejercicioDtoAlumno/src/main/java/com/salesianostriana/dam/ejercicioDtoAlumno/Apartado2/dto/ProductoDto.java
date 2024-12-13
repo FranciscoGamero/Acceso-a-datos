abf037c3055bbc99afd9764399001a3e88f8270d
Francisco_Gamero <126238831+FranciscoGamero@users.noreply.github.com>
2024-12-13 13:36:40 +0100

    Ejercicio DTO Apartado 2

--- Ejercicios/ejercicioDtoAlumno/src/main/java/com/salesianostriana/dam/ejercicioDtoAlumno/Apartado2/dto/ProductoDto.java
+++ Ejercicios/ejercicioDtoAlumno/src/main/java/com/salesianostriana/dam/ejercicioDtoAlumno/Apartado2/dto/ProductoDto.java
@@ -1,4 +1,15 @@
 package com.salesianostriana.dam.ejercicioDtoAlumno.Apartado2.dto;
 
-public record ProductoDto() {
+import com.salesianostriana.dam.ejercicioDtoAlumno.Apartado2.models.Producto;
+
+public record ProductoDto(
+        String nombre,
+        Double pvp,
+        String imagen,
+        String categoria
+) {
+    public static ProductoDto of(Producto producto) {
+        return new ProductoDto(producto.getNombre(), producto.getPvp(),
+                producto.getImagenes().getFirst(), producto.getCategoria().getNombre());
+    }
 }
--- Ejercicios/ejercicioDtoAlumno/src/main/java/com/salesianostriana/dam/ejercicioDtoAlumno/Apartado2/models/Categoria.java
+++ Ejercicios/ejercicioDtoAlumno/src/main/java/com/salesianostriana/dam/ejercicioDtoAlumno/Apartado2/models/Categoria.java
@@ -1,4 +1,13 @@
 package com.salesianostriana.dam.ejercicioDtoAlumno.Apartado2.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Categoria {
+    private Long id;
+    private String nombre;
 }
