2eb8fb7ba6e2709dee9ea1c509675f7f1e7e4c4c
Francisco_Gamero <126238831+FranciscoGamero@users.noreply.github.com>
2025-01-13 12:24:15 +0100

    Ejemplo 2 y 3

--- ejemplosLuismi/02_EjemploBasicoDataJpa/src/main/java/com/salesianos/data/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salesianos.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository
-        extends JpaRepository<Producto, Long> {
-}
